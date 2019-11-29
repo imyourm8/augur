a408a513f9bae7a73c220f6d69c6eb25bfaa4b00
atvanguard <93arpit@gmail.com>
2019-11-29 18:49:51 +0530

    Move out test files

--- packages/augur-core/test/run.sh
+++
@@ -1,6 +0,0 @@
-rm -rf output
-rm test/deployedAddresses test/addresses.json
-node -r ts-node/register source/deployment/compileContracts.ts
-yarn deploy:local > test/deployedAddresses
-node test/addressClipper.js
-node test/predicateTrade.js
