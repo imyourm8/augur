15627e312860c79baa6ea407e2532a46341e4707
Denis Ermolin <denis.ermolin@matic.network>
2020-09-30 15:08:23 +0700

    chg: sync already created market

--- packages/augur-core/source/contracts/matic/AugurSyncer.sol
+++ packages/augur-core/source/contracts/matic/AugurSyncer.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.5.10;
 
 import 'ROOT/reporting/IMarket.sol';
+import 'ROOT/reporting/Market.sol';
 import 'ROOT/reporting/IUniverse.sol';
 import 'ROOT/reporting/Universe.sol';
 import "ROOT/libraries/Ownable.sol";
@@ -12,10 +13,6 @@ contract AugurSyncer is Ownable {
     uint256 private constant SYNC_MARKET_INFO_CMD = 1;
     uint256 private constant SYNC_REPORTING_FEE_CMD = 2;
     uint256 private constant SYNC_MARKET_MIGRATION_CMD = 3;
-
-    // copy from Universe.sol
-    uint256 constant public DEFAULT_NUM_OUTCOMES = 2;
-    uint256 constant public DEFAULT_NUM_TICKS = 100;
 
     IStateSender public stateSender;
     IRegistry public registry;
@@ -32,103 +29,18 @@ contract AugurSyncer is Ownable {
         stateSender = IStateSender(_stateSender);
     }
 
-    function createYesNoMarket(
-        Universe _universe,
-        uint256 _endTime,
-        uint256 _feePerCashInAttoCash,
-        uint256 _affiliateFeeDivisor,
-        address _designatedReporterAddress,
-        string memory _extraInfo
-    ) public returns (IMarket) {
-        IMarket _market = _universe.createYesNoMarket(
-            _endTime,
-            _feePerCashInAttoCash,
-            _affiliateFeeDivisor,
-            _designatedReporterAddress,
-            _extraInfo
-        );
-
+    function syncMarket(Market market) public {
         syncWithCommand(
             SYNC_MARKET_INFO_CMD,
             encodeMarketArguments(
-                _universe,
-                _market,
-                _endTime,
-                _feePerCashInAttoCash,
-                DEFAULT_NUM_TICKS,
-                DEFAULT_NUM_OUTCOMES
+                market.getUniverse(),
+                market,
+                market.getEndTime(),
+                market.getMarketCreatorSettlementFeeDivisor(),
+                market.getNumTicks(),
+                market.getNumberOfOutcomes()
             )
         );
-
-        return _market;
-    }
-
-    function createCategoricalMarket(
-        Universe _universe,
-        uint256 _endTime,
-        uint256 _feePerCashInAttoCash,
-        uint256 _affiliateFeeDivisor,
-        address _designatedReporterAddress,
-        bytes32[] memory _outcomes,
-        string memory _extraInfo
-    ) public returns (IMarket) {
-        IMarket _market = _universe.createCategoricalMarket(
-            _endTime,
-            _feePerCashInAttoCash,
-            _affiliateFeeDivisor,
-            _designatedReporterAddress,
-            _outcomes,
-            _extraInfo
-        );
-
-        syncWithCommand(
-            SYNC_MARKET_INFO_CMD,
-            encodeMarketArguments(
-                _universe,
-                _market,
-                _endTime,
-                _feePerCashInAttoCash,
-                DEFAULT_NUM_TICKS,
-                uint256(_outcomes.length)
-            )
-        );
-
-        return _market;
-    }
-
-    function createScalarMarket(
-        Universe _universe,
-        uint256 _endTime,
-        uint256 _feePerCashInAttoCash,
-        uint256 _affiliateFeeDivisor,
-        address _designatedReporterAddress,
-        int256[] memory _prices,
-        uint256 _numTicks,
-        string memory _extraInfo
-    ) public returns (IMarket) {
-        IMarket _market = _universe.createScalarMarket(
-            _endTime,
-            _feePerCashInAttoCash,
-            _affiliateFeeDivisor,
-            _designatedReporterAddress,
-            _prices,
-            _numTicks,
-            _extraInfo
-        );
-
-        syncWithCommand(
-            SYNC_MARKET_INFO_CMD,
-            encodeMarketArguments(
-                _universe,
-                _market,
-                _endTime,
-                _feePerCashInAttoCash,
-                _numTicks,
-                DEFAULT_NUM_OUTCOMES
-            )
-        );
-
-        return _market;
     }
 
     function migrateMarketIn(
