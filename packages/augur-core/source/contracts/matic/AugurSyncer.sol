0136d8de2ff322542a7ad5376d3fdd885d483c7e
Denis Ermolin <denis.ermolin@matic.network>
2020-10-07 08:53:01 +0700

    chg: some adjustments

--- packages/augur-core/source/contracts/matic/AugurSyncer.sol
+++ packages/augur-core/source/contracts/matic/AugurSyncer.sol
@@ -1,10 +1,10 @@
-pragma solidity 0.5.10;
+pragma solidity 0.5.15;
 
 import 'ROOT/reporting/IMarket.sol';
 import 'ROOT/reporting/Market.sol';
 import 'ROOT/reporting/IUniverse.sol';
 import 'ROOT/reporting/Universe.sol';
-import "ROOT/libraries/Ownable.sol";
+import 'ROOT/libraries/Ownable.sol';
 import 'ROOT/matic/plasma/IStateSender.sol';
 import 'ROOT/matic/plasma/IRegistry.sol';
 import 'ROOT/matic/libraries/BytesLib.sol';
@@ -13,15 +13,18 @@ contract AugurSyncer is Ownable {
     uint256 private constant SYNC_MARKET_INFO_CMD = 1;
     uint256 private constant SYNC_REPORTING_FEE_CMD = 2;
     uint256 private constant SYNC_MARKET_MIGRATION_CMD = 3;
+    uint256 private constant SYNC_MARKET_FINALIZE_CMD = 4;
 
     IStateSender public stateSender;
     IRegistry public registry;
     address public marketRegistry;
 
-    constructor(IRegistry _registry, address _marketRegistry) public {
-        registry = _registry;
+    function setMarketRegistry(address _marketRegistry) public onlyOwner {
         marketRegistry = _marketRegistry;
-        updateChildChainAndStateSender();
+    }
+
+    function setRegistry(IRegistry _registry) public onlyOwner {
+        registry = _registry;
     }
 
     function updateChildChainAndStateSender() public {
@@ -29,16 +32,18 @@ contract AugurSyncer is Ownable {
         stateSender = IStateSender(_stateSender);
     }
 
-    function syncMarket(Market market) public {
+    function syncMarket(Market _market) public {
         syncWithCommand(
             SYNC_MARKET_INFO_CMD,
             encodeMarketArguments(
-                market.getUniverse(),
-                market,
-                market.getEndTime(),
-                market.getMarketCreatorSettlementFeeDivisor(),
-                market.getNumTicks(),
-                market.getNumberOfOutcomes()
+                _market.getUniverse(),
+                _market,
+                _market.getOwner(),
+                _market.getEndTime(),
+                _market.getMarketCreatorSettlementFeeDivisor(),
+                _market.getNumTicks(),
+                _market.getNumberOfOutcomes(),
+                _market.affiliateFeeDivisor()
             )
         );
     }
@@ -52,15 +57,31 @@ contract AugurSyncer is Ownable {
         _universe.migrateMarketIn(_market, _cashBalance, _marketOI);
         syncWithCommand(
             SYNC_MARKET_MIGRATION_CMD,
-            abi.encode(_universe, _market)
+            abi.encode(
+                _universe,
+                _universe.getOrCacheReportingFeeDivisor(),
+                _market
+            )
         );
     }
 
     function syncReportingFee(IUniverse _universe) public onlyOwner {
-        uint256 newFee = _universe.getOrCacheReportingFeeDivisor();
         syncWithCommand(
             SYNC_REPORTING_FEE_CMD,
-            abi.encode(_universe, newFee)
+            abi.encode(_universe, _universe.getOrCacheReportingFeeDivisor())
+        );
+    }
+
+    function syncMarketFinalized(Market _market) public {
+        require(_market.isFinalized(), 'market is not finalized');
+
+        syncWithCommand(
+            SYNC_MARKET_FINALIZE_CMD,
+            abi.encode(
+                _market,
+                _market.isInvalid(),
+                _market.getWinningReportingParticipant().getPayoutNumerators()
+            )
         );
     }
 
@@ -71,12 +92,24 @@ contract AugurSyncer is Ownable {
     function encodeMarketArguments(
         IUniverse universe,
         IMarket market,
+        address owner,
         uint256 endTime,
         uint256 creatorFee,
         uint256 numTicks,
-        uint256 numberOfOutcomes
+        uint256 numberOfOutcomes,
+        uint256 affiliateFeeDivisor
     ) private pure returns (bytes memory) {
-        return abi.encode(universe, market, endTime, creatorFee, numTicks, numberOfOutcomes);
+        return
+            abi.encode(
+                universe,
+                market,
+                owner,
+                endTime,
+                creatorFee,
+                numTicks,
+                numberOfOutcomes,
+                affiliateFeeDivisor
+            );
     }
 
     function onTransferOwnership(address, address) internal {}
